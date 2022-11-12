ccc15e9e82ab4125ec3496df6f85354125fc940a
aniklai <aron.niklai@stud.uni-corvinus.hu>
2022-11-12 10:32:51 +0100

    Add project files.

---
+++ Ramen/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Ramen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listCountries = new System.Windows.Forms.ListBox();
+            this.txtCountryFilter = new System.Windows.Forms.TextBox();
+            this.dgwRamen = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwRamen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listCountries
+            // 
+            this.listCountries.FormattingEnabled = true;
+            this.listCountries.Location = new System.Drawing.Point(12, 67);
+            this.listCountries.Name = "listCountries";
+            this.listCountries.Size = new System.Drawing.Size(216, 355);
+            this.listCountries.TabIndex = 0;
+            this.listCountries.SelectedIndexChanged += new System.EventHandler(this.listCountries_SelectedIndexChanged);
+            // 
+            // txtCountryFilter
+            // 
+            this.txtCountryFilter.Location = new System.Drawing.Point(12, 23);
+            this.txtCountryFilter.Name = "txtCountryFilter";
+            this.txtCountryFilter.Size = new System.Drawing.Size(216, 20);
+            this.txtCountryFilter.TabIndex = 1;
+            this.txtCountryFilter.TextChanged += new System.EventHandler(this.txtCountryFilter_TextChanged);
+            // 
+            // dgwRamen
+            // 
+            this.dgwRamen.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwRamen.Location = new System.Drawing.Point(234, 67);
+            this.dgwRamen.Name = "dgwRamen";
+            this.dgwRamen.Size = new System.Drawing.Size(554, 355);
+            this.dgwRamen.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgwRamen);
+            this.Controls.Add(this.txtCountryFilter);
+            this.Controls.Add(this.listCountries);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgwRamen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listCountries;
+        private System.Windows.Forms.TextBox txtCountryFilter;
+        private System.Windows.Forms.DataGridView dgwRamen;
+    }
+}
+
---
+++ Ramen/Ramen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ramen
+{
+    class Ramen
+    {
+        public int ID { get; set; }
+        public int BrandFK { get; set; }
+        public Brand Brand { get; set; }
+        public string Name { get; set; }
+        public int CountryFK { get; set; }
+        public Country Country { get; set; }
+        public double Rating { get; set; }
+    }
+}
